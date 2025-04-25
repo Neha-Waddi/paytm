40690e780619af5ce563fc083164606d42f8d996
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-04-25 10:43:23 +0530

    mongoose user schema

---
+++ backend/db.js
@@ -0,0 +1,31 @@
+const mongoose=require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/paytm");
+
+const userSchema=mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8 
+    },
+    firstname:{
+        type:String,
+        required:true,
+    },
+    lastname:{
+        type:String,
+        required:true,
+    },
+    // if u want it simple you can write like username:String that's it
+});
+
+const User=mongoose.model("User",userSchema);
+
+module.exports={
+    User
+};
