04aba088aa9fc7013564c09b3cc56fd827814b71
Neha-Waddi <waddineha.22.csm@anits.edu.in>
2025-04-26 13:34:50 +0530

    created account schema

--- backend/db.js
+++ backend/db.js
@@ -24,8 +24,22 @@ const userSchema=mongoose.Schema({
     // if u want it simple you can write like username:String that's it
 });
 
+const accountSchema=new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    balance:{
+        type:Number,
+        required:true
+    }
+});
+
 const User=mongoose.model("User",userSchema);
+const Account=mongoose.model("Account",accountSchema);
+
 
 module.exports={
-    User
+    User,Account
 };
